d07fa477024f891bcbd68222db85e936431b8df0
0x00-0x00 <andre.marques@fatec.sp.gov.br>
2017-01-21 17:56:03 -0200

    Upgraders are now working

--- default/role.upgrader.js
+++ default/role.upgrader.js
@@ -3,15 +3,35 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function (creep)
     {
-        if(creep.carry.energy < creep.carry.carryCapacity)
-        {
-            //var sources = creep.room.find(FIND_SOURCES);
-            var source = creep.pos.findInRange(Game.SOURCES, 3);
-            console.log("Upgrader source: " + source);
-            creep.moveTo(source);
-            creep.harvest(source);
-        }
-        else {
+        if(creep.carry.energy < creep.carryCapacity) {
+
+            /** Gets all sources from ROOM and creates a set for distances **/
+            var sources = creep.room.find(FIND_SOURCES);
+            var distance = [];
+
+            /** Calculate all the distances **/
+            for(var x in sources) {
+                var target = sources[x];
+                var range = creep.pos.getRangeTo(target);
+                distance[x] = range;
+            }
+
+            /** Identify the quickest source available **/
+            var shortest_distance = Array.min(distance);
+            for(var x in sources) {
+                var target = sources[x];
+                var range = creep.pos.getRangeTo(target);
+                if(range == shortest_distance) {
+
+                    /** Moving and harvesting code **/
+                    if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+
+                }
+
+            }
+        } else {
             var target = creep.room.controller;
             if(creep.upgradeController(target) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target);
