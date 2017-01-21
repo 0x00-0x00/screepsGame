bcb204ebaf46bb2007ee00b1aa796a0eaf20f3c1
shemhazai <andre.marques@fatec.sp.gov.br>
2017-01-21 14:23:48 -0200

    First commit

--- default/main.js
+++ default/main.js
@@ -2,6 +2,17 @@ var roleHarvester = require('role.harvester');
 
 module.exports.loop = function () {
 
+    /** Count creeps **/
+    var num_creeps = 0
+    for(var c in Game.creeps) {
+        num_creeps++;
+    }
+
+    if(num_creeps == 0)
+    {
+        Game.spawns['Spawn1'].createCreep([WORK, MOVE, CARRY], 'Harvester');
+    }
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         roleHarvester.run(creep);
--- default/role.harvester.js
+++ default/role.harvester.js
@@ -8,7 +8,7 @@ var roleHarvester = {
   run: function(creep)
   {
       if(creep.carry.energy < creep.carryCapacity) {
-          var sources = creep.room.find(SOURCES)
+          var sources = creep.room.find(FIND_SOURCES)
           if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
               creep.moveTo(sources[0]);
           }
