110cfe285439a7e4a22da7dfe033c9256b5c4d59
0x00-0x00 <andre.marques@fatec.sp.gov.br>
2017-01-28 17:17:48 -0200

    Added a enemy-detection mechanism to avoid kamikazing in enemy creeps

--- default/role.voyager.js
+++ default/role.voyager.js
@@ -2,7 +2,7 @@ var cache = require('role.cache');
 
 let roleVoyager = {
     //parts: [WORK, MOVE, CARRY],
-    parts: [WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY],
+    parts: [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE,],
 
     coming_from: "W2N5",
     destination: "W3N5",
@@ -43,6 +43,7 @@ let roleVoyager = {
 
     run: function(creep) {
         this.creep = creep;
+
         if(creep.memory.working && creep.carry.energy == 0) {
             creep.memory.working = false;
         }
@@ -53,15 +54,41 @@ let roleVoyager = {
 
         if(!creep.memory.working) {
 
-            if(this.voyage(this.destination)) {
+            /** Enemy run away algorithm **/
+            let hostiles = this.creep.room.find(FIND_HOSTILE_CREEPS);
+            if(hostiles.length > 0) {
+                this.creep.memory.seen_enemy = 1;
+                let controller = this.creep.memory.source_room.controller;
+
+                /** Run away from enemy creeps **/
+                if(!this.voyage(this.creep.memory.source_room)) {
+                    this.creep.moveTo(controller, {reusePath: cache.reusePathValue});
+                }
                 return 0;
             }
+
+            /** Freeze the creep with fear from the enemy **/
+            if(this.creep.memory.seen_enemy == 1) {
+                this.creep.memory.seen_enemy_index += 1;
+                if(this.creep.memory.seen_enemy_index % 64 == 0) {
+                    this.creep.memory.seen_enemy = 0;
+                }
+                return 0;
+            }
+
+
+            /** Voyage algorithm **/
+            if(this.voyage(this.destination) && this.creep.memory.seen_enemy == 0) {
+                return 0;
+            }
+
+            /** Harvesting algorithm **/
             let sources = this.creep.room.find(FIND_SOURCES_ACTIVE);
             let nearbySource = this.creep.pos.findClosestByPath(sources);
             this.harvest(nearbySource);
         } else {
 
-            if(this.voyage(this.coming_from)) {
+            if(this.voyage(this.creep.memory.source_room)) {
                 return 0;
             }
 
